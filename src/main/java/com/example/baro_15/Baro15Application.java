e0e7e7b00c6907da5e107fb05a4294079b8cd9dc
seoha <classseoha@gmail.com>
2025-07-25 20:44:52 +0900

    first commit

---
+++ src/main/java/com/example/baro_15/Baro15Application.java
@@ -0,0 +1,13 @@
+package com.example.baro_15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baro15Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Baro15Application.class, args);
+    }
+
+}
